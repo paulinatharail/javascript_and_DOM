b3a626b983d52d008dd431cc09c3510a4b506828
paulina <paulinatharail@yahoo.com>
2019-01-28 13:05:24 -0800

    2 search filters

--- static/js/app.js
+++ static/js/app.js
@@ -9,6 +9,13 @@ var tbody = d3.select("tbody");
 
 
 
+// //function to get filtered data given date and city
+// function dateCity(dateFilter, cityFilter){
+
+// }
+
+
+
 //on Button click event
 filterButton.on("click", function(){
 
@@ -21,27 +28,60 @@ filterButton.on("click", function(){
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
     console.log(inputValue);
+
+    // Get the city filter
+    var inputCity = d3.select("#city");
+    var inputCityValue = inputCity.property("value");
+    console.log(inputCityValue);
     
-    //Filter the data based on the date entered
-    var filteredData = sightings.filter(sight => sight.datetime === inputValue);
-    console.log(filteredData);
+    // debugger;
 
-    //clear any exisiting rows
-    d3.selectAll("#rowData").remove();
+    if (inputCityValue != "" && inputValue != "") {
+        //Filter the data based on the date entered
+        var filteredData = sightings.filter(sight => sight.datetime === inputValue
+            && sight.city === inputCityValue );
+        console.log(filteredData);
 
+        //clear any exisiting rows
+        d3.selectAll("#rowData").remove();
 
-    //populate the table
-    filteredData.forEach((sighting) => {
+        //populate the table
+        filteredData.forEach((sighting) => {
+            
+            // Get a reference to the table body
+            var tbody = d3.select("tbody");
 
-        
-        // Get a reference to the table body
-        var tbody = d3.select("tbody");
-
-        var row = tbody.append("tr").attr("id", "rowData");
-        Object.entries(sighting).forEach(([key, value]) => {
-        var cell = tbody.append("td").attr("id", "rowData");
-        cell.text(value);
+            var row = tbody.append("tr").attr("id", "rowData");
+            Object.entries(sighting).forEach(([key, value]) => {
+            var cell = tbody.append("td").attr("id", "rowData");
+            cell.text(value);
+            });
         });
-    });
+    } 
+    else if (inputValue != ""){
+        console.log("else section entered");
+        //Filter the data based on the date entered
+        var filteredData = sightings.filter(sight => sight.datetime === inputValue);
+        console.log(filteredData);
 
+        //clear any exisiting rows
+        d3.selectAll("#rowData").remove();
+
+
+        //populate the table
+        filteredData.forEach((sighting) => {
+ 
+            // Get a reference to the table body
+            var tbody = d3.select("tbody");
+
+            var row = tbody.append("tr").attr("id", "rowData");
+            Object.entries(sighting).forEach(([key, value]) => {
+            var cell = tbody.append("td").attr("id", "rowData");
+            cell.text(value);
+            });
+        });
+    }
+    else {
+        console.log("Neither criteria met");
+    }
 });
