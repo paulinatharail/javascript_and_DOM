6995f82b3b9d47c7057c85f23ea7a5ba4c0051de
paulina <paulinatharail@yahoo.com>
2019-01-28 16:31:38 -0800

    final

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # javascript_and_DOM
+On page load, the site will display all the data items. 
+
+The filters on the left, when used, displays only the data that meets the filter criteria (date and/or city). The header above the table also changes reflecting the search criteria used.
--- static/js/app.js
+++ static/js/app.js
@@ -7,8 +7,16 @@ var filterButton = d3.select('#filter-btn');
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-
+sightings.forEach((sighting) => {
+    var row = tbody.append("tr").attr("id", "rowData");
+    Object.entries(sighting).forEach(([key, value]) => {
+    var cell = tbody.append("td").attr("id", "rowData");
+    cell.text(value);
+    });
+});
 
 
 //on Button click event
@@ -26,6 +34,9 @@ filterButton.on("click", function(){
     var inputCity = d3.select("#city");
     var inputCityValue = inputCity.property("value");
     console.log(inputCityValue);
+
+    //search criteria
+    var searchCriteria = "";
     
     // debugger;
 
@@ -33,20 +44,26 @@ filterButton.on("click", function(){
         var filteredData = sightings.filter(sight => sight.datetime === inputValue
             && sight.city === inputCityValue );
         console.log(filteredData);      
+        searchCriteria = "Results for " + inputCityValue + " and " + inputValue;
     } 
     else if (inputValue != ""){ //filter only on date
         var filteredData = sightings.filter(sight => sight.datetime === inputValue);
         console.log(filteredData);
+        searchCriteria = "Results for " + inputValue;
     }
     else if (inputCity != "") { //filter only on city
         var filteredData = sightings.filter(sight => sight.city === inputCityValue);
         console.log(filteredData);
+        searchCriteria = "Results for " + inputCityValue;
     }
     
     else {
         console.log("Neither criteria met");
     }
 
+      //Modify the filter header to show filter conditions
+      console.log(searchCriteria);
+      d3.select(".dateheader").text(searchCriteria);
 
       //clear any exisiting rows
       d3.selectAll("#rowData").remove();
